755215ef1b439d2eaad5b5bc1ecff628d3a8373e
KingOfProgrammers2000 <aaahhhmmmeeedddabcdefg@gmail.com>
2025-03-14 08:48:15 +0300

    Add core/urls.py for project routing

--- core/urls.py
+++ core/urls.py
@@ -1,23 +1,29 @@
-"""
-URL configuration for core project.
+# core/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Users endpoints
+    path('api/users/', include('users.urls')),
+
+    # Legal Resources
+    path('api/legalresources/', include('legalresources.urls')),
+
+    # Educational Opportunities
     path('api/educationalopportunities/', include('educationalopportunities.urls')),
+
+    # Favorites
+    path('api/favorites/', include('favorites.urls')),
+
+    # Notifications
+    path('api/notifications/', include('notifications.urls')),
 ]
+
+# If you're using DEBUG mode and want to serve media in dev:
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
