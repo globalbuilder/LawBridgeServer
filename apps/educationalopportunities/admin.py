948f7d91b8112e5c69c08440020aca213bccf399
KingOfProgrammers2000 <aaahhhmmmeeedddabcdefg@gmail.com>
2025-03-14 12:14:41 +0300

    Update admin panels: add clickable file link in legalresources and external link in educationalopportunities

--- apps/educationalopportunities/admin.py
+++ apps/educationalopportunities/admin.py
@@ -1,18 +1,37 @@
+# educationalopportunities/admin.py
+
 from django.contrib import admin
-from .models import EducationalOpportunity
 from django.utils.html import format_html
+from .models import EducationalOpportunity
 
 @admin.register(EducationalOpportunity)
 class EducationalOpportunityAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'preview_image', 'start_date', 'end_date', 'created_at')
+    list_display = (
+        'id', 
+        'title', 
+        'preview_image', 
+        'external_link_display', 
+        'start_date', 
+        'end_date', 
+        'created_at'
+    )
     list_filter = ('start_date', 'end_date',)
     ordering = ('-created_at',)
 
     def preview_image(self, obj):
         if obj.image:
             return format_html(
-                "<img src='{}' style='max-height:50px; max-width:50px;' />",
+                "<img src='{}' style='max-height:50px; max-width:50px;'/>", 
                 obj.image.url
             )
         return "No Image"
     preview_image.short_description = "Image Preview"
+
+    def external_link_display(self, obj):
+        if obj.external_link:
+            return format_html(
+                "<a href='{}' target='_blank'>Open Link</a>", 
+                obj.external_link
+            )
+        return "No Link"
+    external_link_display.short_description = "External Link"
--- apps/legalresources/admin.py
+++ apps/legalresources/admin.py
@@ -1,20 +1,29 @@
 # legalresources/admin.py
 
 from django.contrib import admin
-from .models import LegalResource
 from django.utils.html import format_html
+from .models import LegalResource
 
 @admin.register(LegalResource)
 class LegalResourceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'preview_image', 'created_at')
+    list_display = ('id', 'title', 'preview_image', 'file_link', 'created_at')
     list_filter = ('created_at',)
     ordering = ('-created_at',)
 
     def preview_image(self, obj):
         if obj.image:
             return format_html(
-                "<img src='{}' style='max-height:50px; max-width:50px;' />",
+                "<img src='{}' style='max-height:50px; max-width:50px;'/>", 
                 obj.image.url
             )
         return "No Image"
     preview_image.short_description = "Image Preview"
+
+    def file_link(self, obj):
+        if obj.file:
+            return format_html(
+                "<a href='{}' target='_blank'>Open PDF</a>", 
+                obj.file.url
+            )
+        return "No File"
+    file_link.short_description = "PDF File"
